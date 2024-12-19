8776bec861a32d693e8d986dcf118cc5663b9c91
ewhd <ewhd22@gmail.com>
2024-12-19 07:41:22 -0800

    requests test1

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,23 @@
-import pandas as pd
+# import pandas as pd
+
+
+# def lambda_handler(event, context):
+#     d = {'col1': [1, 2], 'col2': [3, 4]}
+#     df = pd.DataFrame(data=d)
+#     print(df)
+#     print('Done x1.4')
+
+import requests
 
 
 def lambda_handler(event, context):
-    d = {'col1': [1, 2], 'col2': [3, 4]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('Done x1.4')
+    # Make a GET request to a public API
+    response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Print the JSON response
+        print(response.json())
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+    print('Done x1.1')
